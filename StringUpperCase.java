3e18f8539bdfcc3b63f09e93971cbf0b84e5a681
pasupulabhagirathprabas <pbalu5077@gmail.com>
2025-01-30 08:59:00 +0530

    Add files via upload

---
+++ StringUpperCase.java
@@ -0,0 +1,15 @@
+package bhagirath;
+import java.util.*;
+public class StringUpperCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc= new Scanner(System.in);
+String s=sc.next();
+String S= s.toUpperCase();
+System.out.println("Lower Case: " + s);
+System.out.println("Upper Case: " + S);
+
+	}
+
+}
